89a4530d27027ab79aaa0e2d978b87092684610c
tommyroar <isillness@gmail.com>
2016-01-19 17:19:41 +0000

    Maven built

---
+++ java/maven/get-started-1/mvn-get-started/run.sh
@@ -0,0 +1,1 @@
+java -cp target/mvn-get-started-1.0-SNAPSHOT.jar com.tmy.mvn.App
---
+++ java/maven/get-started-1/mvn-get-started/src/main/java/com/tmy/mvn/App.java
@@ -0,0 +1,9 @@
+package com.tmy.mvn;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
