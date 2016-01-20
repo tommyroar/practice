46ef62912b63d37128f4464433ef99e5bb610994
tommyroar <isillness@gmail.com>
2016-01-20 20:14:20 +0000

    Commit

--- java/maven/get-started-1/mvn-get-started/src/main/java/com/tmy/mvn/App.java
+++ java/maven/get-started-1/mvn-get-started/src/main/java/com/tmy/mvn/App.java
@@ -1,9 +1,17 @@
 package com.tmy.mvn;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 public class App 
 {
+
+    //local init logger
+    private static final Logger logger = LogManager.getLogger(App.class);
+
     public static void main( String[] args )
     {
+        logger.error("ERROR");
         System.out.println( "Hello World!" );
     }
 }
